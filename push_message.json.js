8770ba0e37e5ea5d73dccd152c6a98b94c7f002a
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-04-21 16:08:53 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content2":"<em style='color: gold;'>更换背景0.0.2已上传，优化更多细节</em>",
+"content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>",
 "content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }
