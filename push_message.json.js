fb9c1ed3b22b90827ac9bdbc7655174a81113979
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-06-19 01:25:14 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,5 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>",
-"content3":"<em style='color: gold;'>【推】华为智慧屏，价钱优势，全国可发，微信号：XtexNs。好友暗号：【满地星光】【PS.俺不卖机场不搞插件T_T】</em>",
-"content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(原PoiCloud)--月付便宜、丰俭由人】</a></marquee>"
+"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(PoiCloud)--文案不行吸引不到人怎么办】</a></marquee>"
 }
