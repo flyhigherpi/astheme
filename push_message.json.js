157e7424c1b0f23923a5d57abf0083d893c1027b
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-08-04 12:20:56 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,4 +1,4 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>",
+"content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>"
 }
