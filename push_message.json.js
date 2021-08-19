61fb5c088732b1c3097b18977f989120e33901f7
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-08-19 23:19:35 +0800

    ty

--- push_message.json.js
+++ push_message.json.js
@@ -1,4 +1,5 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>"
+"content2":"<em style='color: gold;'>更换背景0.0.4已上传，支持分辨率设置</em>",
+"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联--服务已恢复】</a></marquee>"
 }
