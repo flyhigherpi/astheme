e317387a32f043267940a18c18c2e4767f46d95a
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2022-05-05 13:30:48 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content2":"<em style='color: gold;'>更换背景0.0.6已上传，支持ICON风格选择</em>",
-"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
+"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】优惠码:Ycx2Vfcg 享年付全场85折优惠 优惠截止至2022-05-15日</a></marquee>"
 }
